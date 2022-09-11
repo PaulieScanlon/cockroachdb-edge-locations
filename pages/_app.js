dbabc55a2b2b790a194485df7756fab2ac90391b
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-09-11 19:34:45 +0100

    feat: add seo and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
@@ -5,12 +8,46 @@ const queryClient = new QueryClient();
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
+  const url = 'https://cockroachdb-edge-locations.vercel.app/';
+  const seoTitle = 'Edge';
+  const seoDescription = 'Submit the location of your nearest edge.';
+  const seoImage = 'edge-open-graph-image.jpg';
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <main>
-        <Component {...pageProps} />
-      </main>
-    </QueryClientProvider>
+    <Fragment>
+      <Head>
+        <title>Edge</title>
+        <link rel="canonical" href={url} />
+
+        {/* Primary Meta Tags */}
+        <meta name="title" content={seoTitle} />
+        <meta name="description" content={seoDescription} />
+        <meta name="image" content={`${url}${seoImage}`} />
+
+        {/* Open Graph / Facebook  */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={seoTitle} />
+        <meta property="og:description" content={seoDescription} />
+        <meta property="og:image" content={`${url}${seoImage}`} />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content={url} />
+        <meta name="twitter:title" content={seoTitle} />
+        <meta name="twitter:description" content={seoDescription} />
+        <meta name="twitter:image" content={`${url}${seoImage}`} />
+
+        {/* favicon */}
+        <link rel="icon" type="image/png" sizes="16x16" href={`${url}favicon-16x16.png`} data-react-helmet="true" />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${url}favicon-32x32.png`} data-react-helmet="true" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </QueryClientProvider>
+    </Fragment>
   );
 };
 
