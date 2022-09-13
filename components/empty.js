8c7046c4911948489762296634ded32c4eac0751
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-09-13 21:17:35 +0100

    style: change empty div border colour

--- components/empty.js
+++ components/empty.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Empty = () => {
-  return <div className="rounded border border-border px-2 py-1 text-text"></div>;
+  return <div className="rounded border border-surface px-2 py-1 text-text"></div>;
 };
 
 export default Empty;
