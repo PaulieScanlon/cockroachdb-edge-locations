50c568da858e4b56d8ddd97eb3350061a13c43c3
Paul Scanlon <pauliescanlon@gmail.com>
2023-02-21 07:19:48 +0000

    chore: add error to read-locations response

--- pages/api/read-locations.js
+++ pages/api/read-locations.js
@@ -11,6 +11,6 @@ export default async function handler(req, res) {
       }
     });
   } catch (error) {
-    res.status(500).json({ message: 'Error!' });
+    res.status(500).json({ message: 'Error!', error: error });
   }
 }
