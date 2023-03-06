565a0618dfd0e3386bd727ea2794d12e961b252d
Paul Scanlon <pauliescanlon@gmail.com>
2023-03-06 15:16:45 +0000

    feat: add full name to toggle switch for Vercel

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ const ThreeGlobe = dynamic(() => import('../components/three-globe'), {
 });
 
 const AWS = 'AWS Lambda';
-const VERCEL = 'Vercel';
+const VERCEL = 'Vercel Serverless';
 
 const lambda = [
   fromProvider('eu-central-1', 'AWS'),
@@ -168,7 +168,7 @@ const Page = ({ data }) => {
                   <label className="relative inline-flex items-center cursor-pointer">
                     <input type="checkbox" className="sr-only peer" value={functionProvider} onChange={handleChange} />
                     <div className="w-11 h-6 bg-lambda peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-600 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-serverless"></div>
-                    <span className="ml-3 text-xs font-medium text-primary">{functionProvider}</span>
+                    <span className="ml-2 text-xs font-medium text-primary">{functionProvider}</span>
                   </label>
                   <button
                     className="min-w-[100px] rounded border border-location px-2 py-1 text-location disabled:border-border disabled:text-secondary disabled:cursor-not-allowed hover:text-primary hover:border-primary"
